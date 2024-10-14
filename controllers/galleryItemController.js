1b8cbeaea6bdde6382dc4b184498bfe94f0e34e1
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-10-14 23:14:37 +0530

    authrization on postgalleryitem

--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -1,31 +1,49 @@
 import galleryItem from "../models/galleryItems.js";
 
-export function postgalleryItem(req,res){
-    const bodygalleryItem=req.body;
-    const newgalleryItem= new galleryItem(bodygalleryItem);
-    newgalleryItem.save().then(()=>{
-        res.json({
-            msg:"gallery Item created successfully"
-        })
-    }).catch(
-        ()=>{
-            res.status(404).json({
-                msg:"Item Gallery creation failed"
-            })
-        }
-    )
+export function postgalleryItem(req, res) {
+  const user = req.body.user;
+  //when user is not logged in
+  if (user == null) {
+    res.status(401).json({
+      msg: "Please login to create gallery item",
+    });
+    return;
+  }
+  //check user type
+  if (user.type != "customer") {
+    res.status(403).json({
+      msg: "Only admin can create gallery items",
+    });
+    return;
+  }
+
+  console.log(req.body);
+
+  const bodygalleryItem = req.body.item;
+  const newgalleryItem = new galleryItem(bodygalleryItem);
+  newgalleryItem
+    .save()
+    .then(() => {
+      res.json({
+        msg: "gallery Item created successfully",
+      });
+    })
+    .catch(() => {
+      res.status(404).json({
+        msg: "Item Gallery creation failed",
+      });
+    });
 }
 
-export function getGalleryItems(req,res){
-    galleryItem.find().then(
-        (galleryItemList)=>{
-            res.json(galleryItemList);
-        }
-    ).catch(
-        ()=>{
-            res.status(404).json({
-                msg:"Failed to fetch gallery items"
-            })
-        }
-    )
+export function getGalleryItems(req, res) {
+  galleryItem
+    .find()
+    .then((galleryItemList) => {
+      res.json(galleryItemList);
+    })
+    .catch(() => {
+      res.status(404).json({
+        msg: "Failed to fetch gallery items",
+      });
+    });
 }

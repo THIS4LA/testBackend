292c694f7feda16d6fcc982e9a99202fe131dfad
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-10-21 00:44:53 +0530

    create room model

---
+++ models/room.js
@@ -0,0 +1,43 @@
+import mongoose, { SchemaType } from "mongoose";
+
+const roomSchema = mongoose.Schema({
+    roomId:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    category:{
+        type:String.apply,
+        required: true
+    },
+    maxGuests:{
+        type: Number,
+        required: true,
+        default:3
+    },
+    available:{
+        type: Boolean,
+        required: true,
+        default:true
+    },
+    photos:[
+        {
+            type: String
+        }
+    ],
+    price:{
+        type: mongoose.Types.Decimal128,
+        required: true
+    },
+    specialDescription:{
+        type:String,
+        default:""
+    },
+    notes :{
+        type: String,
+        default:""
+    }
+})
+
+const Room=mongoose.model("rooms",roomSchema);
+export default Room;

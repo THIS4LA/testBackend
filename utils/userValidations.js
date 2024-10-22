09f7a40b7308ef9288757798c7219a8a7f54906f
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-10-22 07:49:58 +0530

    create func to check user loggedornot

--- utils/userValidations.js
+++ utils/userValidations.js
@@ -1,12 +1,19 @@
 export function isAdmin(req, res) {
-    if (!req.user) {
-      res.status(401).json({ msg: "Please login" });
-      return false;
-    }
-  
-    if (req.user.type !== "admin") {
-      res.status(403).json({ msg: "Only admin can use this function" });
-      return false;
-    }
-    return true;
+  if (!req.user) {
+    res.status(401).json({ msg: "Please login" });
+    return false;
   }
+
+  if (req.user.type !== "admin") {
+    res.status(403).json({ msg: "Only admin can use this function" });
+    return false;
+  }
+  return true;
+}
+
+export function isLogged(req, res) {
+  if (!req.user) {
+    res.status(401).json({ msg: "Please login" });
+    return false;
+  }
+}

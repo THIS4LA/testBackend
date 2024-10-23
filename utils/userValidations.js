33f42373035f020a02790ce00ddd18ec55451727
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-10-23 23:36:17 +0530

    isCustomer validation func

--- utils/userValidations.js
+++ utils/userValidations.js
@@ -11,6 +11,19 @@ export function isAdmin(req, res) {
   return true;
 }
 
+export function isCustomer(req, res) {
+  if (!req.user) {
+    res.status(401).json({ msg: "Please login" });
+    return false;
+  }
+
+  if (req.user.type !== "customer") {
+    res.status(403).json({ msg: "Only customer can use this function" });
+    return false;
+  }
+  return true;
+}
+
 export function isLogged(req, res) {
   if (!req.user) {
     res.status(401).json({ msg: "Please login" });
@@ -18,3 +31,4 @@ export function isLogged(req, res) {
   }
   return true;
 }
+
